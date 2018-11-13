5edd7b764ec6882f82307d458e27ee3a945ffe30
willop9 <willop9@googlemail.com>
2018-11-13 21:51:00 +0000

    Correct plot of line and added noise to y value

--- Practical3/Straight_Line_Plot.m
+++ Practical3/Straight_Line_Plot.m
@@ -12,9 +12,13 @@ c = 6;
 mean = 0;
 c = 6;
 
+r = randn(1,samples);
 x = linspace(xStart,xEnd, samples);
-
 y = m*x + c;
+y_noise = y + r(1,:);
 
-plot(x,y,'o');
+figure
+hold on
+plot(x,y_noise,'b-o');
+
 
