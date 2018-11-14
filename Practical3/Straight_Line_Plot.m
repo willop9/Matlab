f3c0f0d5d767db6df2b68c5ada3844e7463c8470
willop9 <willop9@googlemail.com>
2018-11-14 16:30:18 +0000

    Cleaned up code for line, Removed y_noise var

--- Practical3/Straight_Line_Plot.m
+++ Practical3/Straight_Line_Plot.m
@@ -10,16 +10,15 @@ m = 1.6;
 c = 6;
 
 mean = 0;
-c = 6;
 
 r = randn(1,samples);
 x = linspace(xStart,xEnd, samples);
 y = m*x + c;
-y_noise = y + r(1,:);
+y = y + r(1,:);
 
 figure
 hold on
-p = plot(x,y_noise,'b-o');
+p = plot(x,y,'b-o');
 p=title('Linear Data Plot with Noise');
 p=xlabel('Data Point Number');
 p=ylabel('Data Point Value');
